10599d56c12f478c1dab3db3b5bc312b7ac0d446
JaphethDievoleCobbinah <dievole7@gmail.com>
2024-12-31 02:17:09 -0800

    first commit

---
+++ api/index.js
@@ -0,0 +1,18 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+dotenv.config();
+
+mongoose.connect(process.env.MONGODB_URL).then(() => {
+    console.log('Connected to MongoDB!');
+}).catch((err) => {
+   console.log(err)
+});
+
+
+const app = express();
+
+
+app.listen(5000, () => {
+    console.log('Server is running on port 5000');
+});
